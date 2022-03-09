22730a3fff94a53bcd179c12a822d48b874cc396
birhanus <birhanushimles716@gmail.com>
2022-03-09 17:17:43 +0000

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ import models
 from uuid import uuid4
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column
+from sqlalchemy import Column, Integer
 from sqlalchemy import DateTime
 from sqlalchemy import String
 
