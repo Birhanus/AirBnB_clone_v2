e536ef3fd31b63937965e3fd9dec23764712b7a3
birhanus <birhanushimles716@gmail.com>
2022-04-08 13:26:58 +0000

    Hello Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,18 @@
 #!/usr/bin/pyhon3
-"""Script tha starts a Flask web application"""
+"""Script tha starts a Flask web application
+"""
 
 from flask import Flask
 
 
 app = Flask(__name__)
 
-app.url_map.strict_slashes = False
 
-
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def display():
+    """Display Hello HBNB!"""
     return "<p>Hello HBNB!</p>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
